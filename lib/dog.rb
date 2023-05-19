9102f3bdf77c93d7149379f9b8c4ec09db31f4ec
John <john.muchuku@student.moringaschool.com>
2023-05-19 09:19:50 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
 
+ # Getter method for name
+ def name
+    @name
+  end
+
+  # Setter method for name
+  def name=(new_name)
+    @name = new_name
+  end
+
+  # Getter method for breed
+  def breed
+    @breed
+  end
+
+  # Setter method for breed
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+
+
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
 
+ # Getter method for name
+ def name
+    @name
+  end
+
+  # Setter method for name
+  def name=(new_name)
+    @name = new_name
+  end
+
+  # Getter method for job
+  def job
+    @job
+  end
+
+  # Setter method for job
+  def job=(new_job)
+    @job = new_job
+  end
+  
 end
